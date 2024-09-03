67e0853199f13aece51eaa7f0194de3c0c9de784
Ryan Lin <linjinhong@yandex.com>
2024-09-03 16:12:03 -0400

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/cw-04-todo-app-starter/", /* TODO: update to your repository name */
+  base: "todo-app", /* TODO: update to your repository name */
 });
